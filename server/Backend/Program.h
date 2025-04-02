8576e83ed6e72e9a3304c2c3636689326bba64a9
kyo <yoonjunkyo@gmail.com>
2025-04-02 20:38:46 +0900

    update project

---
+++ server/Backend/Program.h
@@ -0,0 +1,14 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+#include<iostream>
+
+class Program {
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+#endif //PROGRAM_H
---
+++ server/InferenceEngine/AlgorithmicObject.h
@@ -0,0 +1,12 @@
+#ifndef ALGORITHMICOBJECT_H
+#define ALGORITHMICOBJECT_H
+
+
+
+class AlgorithmicObject {
+
+};
+
+
+
+#endif //ALGORITHMICOBJECT_H
