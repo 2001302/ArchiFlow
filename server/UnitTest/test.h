4bfe41b7827494067d23360c06dc51db0d852a1a
kyo <yoonjunkyo@gmail.com>
2025-04-05 01:24:54 +0900

    add unit test

---
+++ server/UnitTest/test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+#include<iostream>
+#include<gtest/gtest.h>
+
+class test {
+
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+#endif //TEST_H
